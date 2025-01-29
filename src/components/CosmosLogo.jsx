03b38e4ce4c104c2d18ae945203187e9c97385e0
Gildo-Henriques <gildohenriques520@gmail.com>
2025-01-29 02:04:54 +0100

    Ultimo commit

--- src/components/CosmosLogo.jsx
+++ src/components/CosmosLogo.jsx
@@ -1,7 +1,7 @@
 export default function CosmosLogo() {
   return (
     <>
-      <img src="/public/COSMOS.png" alt="" className="object-cover rounded-full size-10" />
+      <img src="https://github.com/Gildo-Henriques/twitter-copia/blob/main/public/COSMOS.png?raw=true" alt="" className="object-cover rounded-full size-10" />
     </>
   );
 }
